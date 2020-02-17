0ee61e77ba733962064aa1c73c566034a30d75b6
Shannon McKinney <13708207+smckinney1@users.noreply.github.com>
2020-02-17 16:18:05 -0500

    Added mutation to add a link to feed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
-  return <LinkList />;
+  return (
+    <>
+      <CreateLink />
+      <LinkList />
+    </>
+  );
 }
 
 export default App;
