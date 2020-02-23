271e63f728ccb38f2df7cf970b9392c88d121d1b
Shannon McKinney <13708207+smckinney1@users.noreply.github.com>
2020-02-23 14:39:54 -0500

    Middleware installed to authenticate all API calls

--- src/index.js
+++ src/index.js
@@ -6,17 +6,30 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { AUTH_TOKEN } from './constants';
 import './styles/index.css';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
 
+// NOTE: This middleware adds the auth token to all calls before making requests to the server.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
