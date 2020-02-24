bde2c2817ebf91d7c4b283673a456181190afd45
Shannon McKinney <13708207+smckinney1@users.noreply.github.com>
2020-02-24 12:39:02 -0500

    Link and vote subscriptions set up

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -27,7 +27,82 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 const LinkList = () => {
+  const subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData) return prev;
+
+        const { newLink } = subscriptionData.data;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+
+        if (exists) return prev;
+
+        return {
+          ...prev,
+          feed: {
+            links: [...prev.feed.links, newLink],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        };
+      },
+    });
+  };
+
+  const subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({ document: NEW_VOTES_SUBSCRIPTION });
+  };
+
   const updateStoreAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY });
     const votedLink = data.feed.links.find(link => link.id === linkId);
@@ -38,9 +113,12 @@ const LinkList = () => {
 
   return (
     <Query query={FEED_QUERY}>
-      {({ loading, error, data }) => {
+      {({ loading, error, data, subscribeToMore }) => {
         if (loading) return <div>Loading...</div>;
         if (error) return <div>Error :(</div>;
+
+        subscribeToNewLinks(subscribeToMore);
+        subscribeToNewVotes(subscribeToMore);
 
         const { links } = data.feed;
 
